4b7909891dd4eb34c2ee634e54e07fb70e682159
Raul Zarate <101022036+zaratr@users.noreply.github.com>
2023-04-20 10:13:54 -0700

    syntax error and commented variable data out-solved

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-let data = require('./data/petdata');
+//let data = require('./data/petdata');
 let weatherData = require('./data/weather');
 const axios = require('axios');
 
